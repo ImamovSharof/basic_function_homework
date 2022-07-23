bc176eed6d0666aa68ad40c66f12ebe16b537013
sharof <sharofimamov3@gmail.com>
2022-07-23 12:03:06 +0500

    001

--- bin/basic_func01.dart
+++ bin/basic_func01.dart
@@ -1,4 +1,14 @@
 // Create a function called func.
 
 //  Return the value 0
-void main() {}
+int func(){
+    return 0;
+}
+
+void main() {
+    int x;
+    x=func();
+    print(x);
+    // print(func());
+
+}
